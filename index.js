2a73f9fb39e07304ff308b95c1d9662d5e4c6008
Prateek Varshney <varshneyprateek20@gmail.com>
2021-01-17 14:37:31 +0530

    Job Posting and more

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const PORT = process.env.PORT || 5000;
 const {MONGOURI} = require('./config/keys');
 const user = require('./routes/Auth');
 const product = require('./routes/product');
+const job = require('./routes/job');
 
 app.use(express.json());
 
 app.use('/user',user);
 app.use('/products',product);
+app.use('/job',job);
 
 mongoose.connect(MONGOURI,{ useNewUrlParser:true,useUnifiedTopology: true  })
     .then(() => {
--- routes/product.js
+++ routes/product.js
@@ -14,23 +14,20 @@ router.get('/',(req,res) => {
     })
 })
 
-router.post('/create',requireLogin,(req,res)=>{
-    const {name,description,image,price,quantity,address,contactNo,Days_For_Rent,category,babyage} = req.body 
-    if(!name || !description || !image || !price || !quantity || !address || !contactNo || !category){
+router.post('/',requireLogin,(req,res)=> {
+    const {name,description,image,price,quantity,Days_For_Rent,address,contactNo,category} = req.body 
+    if(!name || !description || !image || !price || !quantity|| !Days_For_Rent || !address || !contactNo || !category){
       return  res.status(422).json({error:"Plase add all the fields"})
     }
-    req.user.password = undefined
     const product = new Product({
-        name,
-        description,
+        name,description,
         image,
         price,
         quantity,
+        Days_For_Rent,
         address,
         contactNo,
-        Days_For_Rent,
         category,
-        babyage,
         user:req.user
     })
     product.save()
@@ -39,6 +36,53 @@ router.post('/create',requireLogin,(req,res)=>{
     })
     .catch(err=>{
         console.log(err)
+    })
+})
+
+router.get('/:id',(req,res) => {
+    const {id} = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);    
+    Product.findOne({_id:id})
+    .exec((err,product) => {
+        if(err || !product){
+            return res.status(422).json({error:err})
+        }
+        res.json({product})        
+    })
+})
+
+router.put('/:id',requireLogin,(req,res) => {
+    const {id} = req.params;
+    const {name,description,image,price,quantity,address,contactNo,Days_For_Rent,category,babyage} = req.body;
+    const updatedProduct = {name,description,image,price,quantity,address,contactNo,Days_For_Rent,category,babyage,_id:id};
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);    
+    Product.findByIdAndUpdate(id,updatedProduct,{new:true})
+    .exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+router.delete('/:id',requireLogin,(req,res)=>{
+    const {id} = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);    
+    Product.findOne({_id:id})
+    .populate("user","_id")
+    .exec((err,product)=>{
+        if(err || !product){
+            return res.status(422).json({error:err})
+        }
+        if(product.user._id.toString() === req.user._id.toString()){
+              product.remove()
+              .then(result=>{
+                  res.json(result)
+              }).catch(err=>{
+                  console.log(err)
+              })
+        }
     })
 })
 
